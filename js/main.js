59acf40c697ef801d15c02036fe69ae8690ce86d
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-04-27 14:48:30 +0200

    base structure of html, css and js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Dichiare le variabili globali
+
+
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere 
+
+
+// Il programma dovrà chiedere all’utente la sua età
+
+
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+
+// Va applicato uno sconto del 20% per i minorenni
+
+
+// Va applicato uno sconto del 40% per gli over 65.
+
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
+
+
+// Output
+
